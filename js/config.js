7d2bd00ccdcba02e910bd2e6feb8431028f4fb48
qixiao <89719729+qixiaodi@users.noreply.github.com>
2025-11-18 15:28:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -26,8 +26,8 @@ const API_SITES = {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
         adult: true
-
-        dyttzy: {
+    },
+    dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com', 
@@ -121,7 +121,6 @@ const API_SITES = {
     },
 
 
-    }
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
